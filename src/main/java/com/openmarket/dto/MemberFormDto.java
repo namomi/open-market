cc1dd4da5a7fbcabf0c53f4a698834a00326716d
namomi <xicodey@gmail.com>
2024-01-08 17:58:25 +0900

    Completed implementation of membership registration function

--- src/main/java/com/openmarket/dto/MemberFormDto.java
+++ src/main/java/com/openmarket/dto/MemberFormDto.java
@@ -1,13 +1,26 @@
 package com.openmarket.dto;
 
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
-@Getter
-@RequiredArgsConstructor
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Data
 public class MemberFormDto {
-    private final String name;
-    private final String email;
-    private final String password;
-    private final String address;
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String name;
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    private String email;
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
+    private String password;
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
+    private String address;
+
 }
